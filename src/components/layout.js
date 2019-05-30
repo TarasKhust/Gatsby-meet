86fc2e53ea619e4ece0b584c303f8b6073428a63
Taras Rushchak <taraskhust@gmail.com>
2019-05-30 18:59:47 +0300

    add image plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,62 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
+import { StaticQuery, graphql } from 'gatsby';
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
+import './layout.css';
 import Archive from './archive';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Main = styled.main `
+const Main = styled.main`
     margin: 0 auto;
     max-Width: 960px;
     padding: 0px 1.0875rem 1.45rem;
     text-decoration: none;
     list-style: none;
-`
+`;
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+		<StaticQuery
+				query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
             title
           }
         }
+        file(relativePath: {
+    regex: "/bg/"
+  }) {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
       }
     `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <Main>
-          <main>{children}
-            <Archive/>
-          </main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </Main>
-      </>
-    )}
-  />
-)
+				render={data => (
+						<>
+							<Header siteTitle={data.site.siteMetadata.title}/>
+							<Img fluid={data.file.childImageSharp.fluid}/>
+							<Main>
+								<main>{children}
+									<Archive/>
+								</main>
+								<footer>
+									© {new Date().getFullYear()}, Built with
+									{` `}
+									<a href="https://www.gatsbyjs.org">Gatsby</a>
+								</footer>
+							</Main>
+						</>
+				)}
+		/>
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+	children: PropTypes.node.isRequired,
+};
 
-export default Layout
+export default Layout;
