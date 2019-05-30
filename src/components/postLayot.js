3d43631bd70974e2e556cf866d58f5f8fb01c64e
Taras Rushchak <taraskhust@gmail.com>
2019-05-30 11:44:31 +0300

    api create pages

---
+++ src/components/postLayot.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from './layout';
+
+class PostLayot extends Component {
+	render() {
+		return (
+				<Layout>
+					<h1>Post Layot</h1>
+				</Layout>
+		);
+	}
+}
+
+export default PostLayot;
