d68f7ee3f5f72ff84a461879bdba06ecca6ffaca
Taras Rushchak <taraskhust@gmail.com>
2019-05-30 22:28:48 +0300

    adds netlify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,8 @@ module.exports = {
         name: 'images'
       }
     },
-      'gatsby-transformer-remark'
+      'gatsby-transformer-remark',
+      'gatsby-plugin-netlify'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`
