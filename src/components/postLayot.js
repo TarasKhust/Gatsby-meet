d2a9ba638c7e9718357ff95b62cd301676a7be32
Taras Rushchak <taraskhust@gmail.com>
2019-05-30 15:02:28 +0300

    api qraphql

--- src/components/postLayot.js
+++ src/components/postLayot.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { graphql } from 'gatsby';
 import Layout from './layout';
 
-class PostLayot extends Component {
+// Static Query
+// Used anywhere, doesn't accept variable, can't use context
+
+// Page Query
+// Must be used on pages
+
+export default class postLayot extends Component {
 	render() {
+		const { markdownRemark } = this.props.data;
+
 		return (
 				<Layout>
-					<h1>Post Layot</h1>
+					<h1>{markdownRemark.frontmatter.title}</h1>
+					<div dangerouslySetInnerHTML={{
+						__html: markdownRemark.html
+					}} />
 				</Layout>
-		);
+		)
 	}
 }
 
-export default PostLayot;
+export const query = graphql`
+	query PostQuery($slug: String!) {
+		markdownRemark(frontmatter: {
+			slug: {
+				eq: $slug
+			}
+		}) {
+			html
+			frontmatter {
+				title
+				date
+				slug
+			}
+		}
+	}
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
+import Listing from '../components/listing';
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Listing/>
   </Layout>
 )
 
