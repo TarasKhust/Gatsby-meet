f6a50ba7edd90ca580917a767680239f591b8a3e
Taras Rushchak <taraskhust@gmail.com>
2019-05-31 08:24:07 +0300

    add admin plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,8 @@ module.exports = {
       }
     },
       'gatsby-transformer-remark',
-      'gatsby-plugin-netlify'
+      'gatsby-plugin-netlify',
+      `gatsby-plugin-netlify-cms`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`
